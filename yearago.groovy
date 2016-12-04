d4782c007705026f823f43a16639942706a1947d
John Cotter <jdcotter@gmail.com>
2016-12-04 10:49:12 -0600

    clean up unused code in yearago.groovy

--- yearago.groovy
+++ yearago.groovy
@@ -1,22 +1,13 @@
 #!/usr/bin/env groovy
 import groovy.time.TimeCategory
 
-def years = [1..10]
-// yearAgo = Date.parse("t - 1 y").toString('yyyy/mm/dd');
-//today = new Date()
-//yearAgo = Date.today().add(-numYears).years().toString('yyyy/MM/dd');
-//yearAgoOneDay = Date.today().add( {days: -numYears, years: -numYears}).toString('yyyy/MM/dd');
-//beforeAfter = "after:" + yearAgoOneDay + " before:" + yearAgo;kk
-
-
-
 def acceptedFormat = "yyyy/MM/dd"
 def today = new Date() + 1
 def currentdate = today.format(acceptedFormat)
 println("Today is $currentdate")
 
-(1..10).each { year ->
-    println " Processing $year"
+(1..15).each { year ->
+    println " Processing $year ago..." 
     use(TimeCategory) {
         def lastyear = today - year.year
         def yesteryear = lastyear + 1.day
